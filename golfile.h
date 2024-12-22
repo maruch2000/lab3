793de6639d5fcb87f675064e86ddab3fc42fcf38
maruch2000 <markinegornov@gmail.com>
2024-12-22 20:54:58 +0300

    Create golfile.h

---
+++ golfile.h
@@ -0,0 +1,26 @@
+#ifndef GOLFILE_H
+#define GOLFILE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char* model;
+    float price;
+    int year;
+} Laptop;
+
+void init_database();
+void loadFile(const char* filename);
+void saveFile(const char* filename);
+void add(Laptop laptop);
+Laptop* get_laptops(int* count);
+void freeMem(Laptop* laptops, int count);
+void deleteLaptop(int index);
+void editLaptop(int index, const char* new_model, float new_price, int new_year);
+void searchLaptop(const char* model, float min_price, float max_price, Laptop** results, int* result_count);
+void menu();
+void userMenu();
+
+#endif
